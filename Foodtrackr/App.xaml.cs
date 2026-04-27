d8c88a6a27f0de08e4732ce2bf06872197c5f083
taylo <taylo@GODPC>
2026-04-27 12:02:49 +1200

    Add project files.

---
+++ Foodtrackr/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Foodtrackr
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Foodtrackr/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Foodtrackr
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
